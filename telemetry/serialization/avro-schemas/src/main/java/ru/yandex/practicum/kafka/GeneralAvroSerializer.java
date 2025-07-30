a59d6a49f22dbba39f3f23ea320bc67a0dc6f1ef
itsmekatrine <itsmekatrine1@gmail.com>
2025-07-30 23:09:05 +0300

    Добавление локального сериализатора

---
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/GeneralAvroSerializer.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.kafka;
+
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+public class GeneralAvroSerializer<T extends SpecificRecord> implements Serializer<T> {
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) { }
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        if (data == null) return null;
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            DatumWriter<T> writer = new SpecificDatumWriter<>(data.getSchema());
+            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+            writer.write(data, encoder);
+            encoder.flush();
+            return out.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing Avro message", e);
+        }
+    }
+
+    @Override
+    public void close() { }
+}
