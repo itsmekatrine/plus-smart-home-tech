df8e1e8a50377c2587c949ef180148392efdcd6e
itsmekatrine <itsmekatrine1@gmail.com>
2025-07-30 21:22:16 +0300

    Добавление классов

--- telemetry/collector/src/main/java/ru/smarthome/collector/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/smarthome/collector/controller/CollectorController.java
@@ -2,6 +2,7 @@ package ru.smarthome.collector.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,14 @@ public class CollectorController {
     private final CollectorService service;
 
     @PostMapping("/sensors")
-    public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+    public ResponseEntity<Void> collectSensorEvent(@Valid @RequestBody SensorEvent event) {
         service.collectSensorEvent(event);
+        return ResponseEntity.accepted().build();
     }
 
     @PostMapping("/hubs")
-    public void collectHubEvent(@RequestBody HubEvent event) {
+    public ResponseEntity<Void> collectHubEvent(@RequestBody HubEvent event) {
         service.collectHubEvent(event);
+        return ResponseEntity.accepted().build();
     }
 }
