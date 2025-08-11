93fe62b2a71c97ef671ef807df1d7dc307a480ae
itsmekatrine <itsmekatrine1@gmail.com>
2025-08-11 19:49:31 +0300

    - Добавление схем снапшотов - Добавление десериализатора

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregationStarter.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AggregationStarter {
+}
