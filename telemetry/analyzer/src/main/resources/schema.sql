f62b7279a4c0f96f0e1d42317e62b1b2be63b921
itsmekatrine <itsmekatrine1@gmail.com>
2025-08-18 11:25:10 +0300

    Исправления для тестов

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -43,41 +43,32 @@ CREATE TABLE IF NOT EXISTS scenario_actions (
     PRIMARY KEY (scenario_id, action_id)
 );
 
--- проверка соответствия hub_id для actions
+-- создаём функцию для проверки, что связываемые сценарий и датчик работают с одним и тем же хабом
 CREATE OR REPLACE FUNCTION check_action_hub_id()
 RETURNS TRIGGER AS
-$$
+'
 BEGIN
-    IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) <>
-       (SELECT s.hub_id
-          FROM actions a
-          JOIN sensors s ON s.id = a.sensor_id
-         WHERE a.id = NEW.action_id)
-    THEN
-        RAISE EXCEPTION 'Hub IDs do not match for scenario_id % and action_id %',
-                        NEW.scenario_id, NEW.action_id;
+    IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) != (SELECT s.hub_id FROM actions a, sensors s
+      WHERE a.id = NEW.action_id and s.id = a.sensor_id) THEN
+        RAISE EXCEPTION ''Hub IDs do not match for scenario_id and sensor_id'';
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+'
+LANGUAGE plpgsql;
 
--- проверка соответствия hub_id для conditions
 CREATE OR REPLACE FUNCTION check_condition_hub_id()
 RETURNS TRIGGER AS
-$$
+'
 BEGIN
-    IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) <>
-       (SELECT s.hub_id
-          FROM conditions c
-          JOIN sensors s ON s.id = c.sensor_id
-         WHERE c.id = NEW.condition_id)
-    THEN
-        RAISE EXCEPTION 'Hub IDs do not match for scenario_id % and condition_id %',
-                        NEW.scenario_id, NEW.condition_id;
+    IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) != (SELECT s.hub_id FROM conditions c, sensors s
+      WHERE c.id = NEW.condition_id and s.id = c.sensor_id) THEN
+        RAISE EXCEPTION ''Hub IDs do not match for scenario_id and sensor_id'';
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+'
+LANGUAGE plpgsql;
 
 -- создаём триггер, проверяющий, что «условие» связывает корректные сценарий и датчик
 CREATE OR REPLACE TRIGGER tr_bi_scenario_conditions_hub_id_check
