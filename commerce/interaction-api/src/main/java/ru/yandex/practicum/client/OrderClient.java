d00e2e5b7a9cb668157cbc00ebc86a186bf2e1df
itsmekatrine <itsmekatrine1@gmail.com>
2025-09-21 12:15:14 +0300

    Добавление аннотации @Valid

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/client/OrderClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/client/OrderClient.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.client;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -17,10 +18,10 @@ public interface OrderClient {
     List<OrderDto> getClientOrders(@RequestParam(name = "username") @NotBlank String userName);
 
     @PutMapping
-    OrderDto createNewOrder(@RequestBody CreateOrderRequest request);
+    OrderDto createNewOrder(@Valid @RequestBody CreateOrderRequest request);
 
     @PostMapping("/return")
-    OrderDto returnProducts(@RequestBody ProductReturnRequest request);
+    OrderDto returnProducts(@Valid @RequestBody ProductReturnRequest request);
 
     @PostMapping("/payment")
     OrderDto payOrder(@RequestBody @NotNull UUID orderId);
